04eedf20c82af591fde1a78d716c9dd7fb7cf58e
Kei Hibino <ex8k.hibino@gmail.com>
2016-02-05 09:36:52 +0900

    Avoid empty package.

---
+++ compat/Data/ByteString/Short/Compat.hs
@@ -0,0 +1,16 @@
+-- |
+-- Module      : Data.ByteString.Short.Compat
+-- Copyright   : (c) Kei Hibino 2015-2016
+-- License     : BSD3
+--
+-- Maintainer  : ex8k.hibino@gmail.com
+-- Stability   : experimental
+-- Portability : ghc only
+--
+-- Avoiding empty package and explicitly compatible module name.
+-- May use the name "Data.ByteString.Short" implicitly.
+module Data.ByteString.Short.Compat
+       ( module Data.ByteString.Short
+       ) where
+
+import Data.ByteString.Short
